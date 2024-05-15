a72abd7309106df93370d0c39e17610a54dc819f
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-05-15 15:02:04 +0200

    image and font

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,18 @@
 import Navigation from "../Navigation"
 import Hero from "../Hero"
+import { Oswald } from "next/font/google"
+import { Lora } from "next/font/google"
 import styles from './Header.module.css'
 
+const oswald = Oswald({subsets: ["latin"]})
+const lora = Lora({subsets: ["latin"]})
+
 const Header = () => {
+
     return(
         <header className={styles.header}>
             <Hero />
-            <h1>Power mom</h1>
+                <h1 className={lora.className}>Power mom</h1>
             <Navigation />
         </header>
     )
