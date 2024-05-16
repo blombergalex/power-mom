1bae1033bd28542d1614ea4fbcea64bf03a11c60
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-05-16 10:28:40 +0200

    remove device colour scheme adjusting

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -3,11 +3,12 @@ import Poppies from '../../../public/images/poppies.jpg'
 
 const Hero = () => {
     return (
-        <Image src={Poppies} alt="Poppies"
-        style={{
+        <Image src={Poppies} alt="Poppies" 
+          style={{
             width: "100%",
-            height: "auto"
-        }}/>
+            height: "100vh",
+            objectFit: "cover"
+        }}/>      
     )
 }
 
