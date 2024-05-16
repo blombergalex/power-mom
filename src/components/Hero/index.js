0e5c99dc4b740ab2dc94a2315b21cb66e46d9798
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-05-16 10:48:39 +0200

    welcome message on header

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,14 +1,25 @@
 import Image from 'next/image'
 import Poppies from '../../../public/images/poppies.jpg'
+import { Dancing_Script } from 'next/font/google'
+import styles from './Hero.module.css'
+
+const dancingScript = Dancing_Script({subsets: ["latin"]})
 
 const Hero = () => {
     return (
-        <Image src={Poppies} alt="Poppies" 
-          style={{
-            width: "100%",
-            height: "100vh",
-            objectFit: "cover"
-        }}/>      
+        <div className={styles.hero}>
+            <div className={styles.heroImage}>
+                <Image
+                    src={Poppies}
+                    alt="Poppies"
+                    layout="fill"
+                    objectFit="cover"
+                />
+            </div>
+            <div className={styles.welcomeMessage}>
+                <h2 className={dancingScript.className}>Welcome to Power Mom</h2>
+            </div>
+        </div>
     )
 }
 
